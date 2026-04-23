e55a087353119567bee1e69fdccd0372466ebb78
Otycadet <Otoabasi.cadet@gmail.com>
2026-04-23 09:44:29 +0100

    update to login sessions

--- aristoclean/settings.py
+++ aristoclean/settings.py
@@ -89,6 +89,8 @@ AUTH_PASSWORD_VALIDATORS = [
 LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
+SESSION_COOKIE_AGE = int(os.environ.get("SESSION_COOKIE_AGE", "1200"))
+SESSION_SAVE_EVERY_REQUEST = True
 
 # ── Internationalisation ───────────────────────────────────────────────────
 LANGUAGE_CODE = "en-us"
